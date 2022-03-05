5fe77aaa1c03caa9f181cbc2a24c1a7606836afb
melodytowett <melodytowett99@gmail.com>
2022-03-05 15:14:56 +0300

    add password property

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
-from . import db
+from sqlalchemy import false
+from . import db,login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 
 
  
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -19,7 +20,22 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
-    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Review',backref = 'user',lazy = "dynamic")
+
+    '''
+    decorator to create a write only class property password
+    '''
+    @property
+    def password(self):
+        raise AttributeError('Password attribute cannot be read')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
 
     def __repr__(self):
         return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User,Role = Role)
+    return dict(app = app, db = db, User = User)
 
 
 if __name__ == '__main__':
