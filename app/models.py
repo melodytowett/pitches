9b90e8fe4390872ca3696c021c8d458d831d63af
melodytowett <melodytowett99@gmail.com>
2022-03-05 14:07:06 +0300

    creating userclass

--- app/models.py
+++ app/models.py
@@ -1,13 +1,19 @@
-from .import db,login_manager
+from typing_extensions import Self
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 
 
  
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
+    id = db.Column(db.Interger,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -1,4 +1,21 @@
-from app import app
+from multiprocessing import Manager
+from flask import app
+from app import create_app,db
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = create_app('prduction')
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, user=User)
+
 if __name__ =="__main__":
-    app.run()
+    manager.run()
 
